067f5053d217e9d748750af4320fb2a09fff8748
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-09-04 14:27:23 +0000

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
-export default function Home() {
+import Social from "@/components/Social";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main className="flex justify-center items-center h-screen font-bold">Home</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1 mb-6">
+              Hello I&apos;m <br />
+              <span className="text-accentNeon">Yana Suryana</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a passionate software engineer, crafting thoughtful solutions
+              with a focus on efficiency and reliability, while staying
+              adaptable to new technologies and methods.
+            </p>
+          </div>
+
+          {/* Button and Socials */}
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button
+              variant="outline"
+              size="lg"
+              className="uppercase flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+            <div className="mb-8 xl:mb-0">
+              <Social />
+            </div>
+          </div>
+        </div>
+
+        {/* photo */}
+        <div>Photo</div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
