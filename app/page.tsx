3cb6c500eb6828c81d43686eaefcdbaa06081350
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-09-05 19:26:27 +0000

    Add motion circle to photo

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,9 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/* text */}
-          <div className="text-center xl:text-left">
+          
+          {/* text + button + socials */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />
@@ -20,32 +21,32 @@ const Home = () => {
               with a focus on efficiency and reliability, while staying
               adaptable to new technologies and methods.
             </p>
-          </div>
 
-          {/* Button and Socials */}
-          <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button
-              variant="outline"
-              size="lg"
-              className="uppercase flex items-center gap-2"
-            >
-              <span>Download CV</span>
-              <FiDownload className="text-xl" />
-            </Button>
-            <div className="mb-8 xl:mb-0">
-              <Social
-                containerStyle="flex gap-6"
-                iconStyle="w-9 h-9 border border-accentNeon rounded-full flex justify-center 
-                items-center text-accentNeon text-base hover:bg-accentNeon hover:text-primary 
-                transition-all duration-500"
-              />
+            {/* Button and Socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyle="flex gap-6"
+                  iconStyle="w-9 h-9 border border-accentNeon rounded-full flex justify-center 
+                  items-center text-accentNeon text-base hover:bg-accentNeon hover:text-primary 
+                  transition-all duration-500"
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* photo */}
-        <div>
-          <Photo/>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,6 +1,82 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
   return (
-    <div>Photo</div>
+    <div className="w-full h-full relative flex justify-center items-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: {
+            delay: 2,
+            duration: 0.4,
+            ease: "easeIn",
+          },
+        }}
+        className="relative flex justify-center items-center"
+      >
+        {/* image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: 2.4,
+              duration: 0.4,
+              ease: "easeInOut",
+            },
+          }}
+          className="absolute w-[280px] h-[280px] xl:w-[480px] xl:h-[480px] rounded-full overflow-hidden flex justify-center items-center"
+        >
+          {/* Blur ring */}
+          <div className="absolute inset-0 rounded-full ring-8 ring-white/20 blur-3xl opacity-70 pointer-events-none"></div>
+
+          <Image
+            src="/assets/profile.png"
+            priority
+            quality={100}
+            fill
+            alt="Profile photo"
+            className="object-cover rounded-full shadow-2xl"
+          />
+        </motion.div>
+
+        {/* circle */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: [
+                "15 120 25 25",
+                "16 25 92 72",
+                "4 250 22 22",
+              ],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </motion.div>
+    </div>
   );
 };
 
